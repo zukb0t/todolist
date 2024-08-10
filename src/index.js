28849ced8ee209128aca8707a7c7d797866a56c0
Duong Tang <dttang1994@gmail.com>
2024-08-09 17:36:12 -0700

    got edit and remove task working.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
-import addProject from './projectproperties.js';
-import addTask from './taskproperties.js';
-import displayProjects, {displayDefault} from './interface.js';
+import {displayDefault} from './interface.js';
 import addProjectList, {checkToday,checkWeek} from './eventhandlers.js';
-import {add, lightFormat} from 'date-fns';
 import './style.css';
 
-//addProject("Runescape");
-//addTask(0,"colosseum","sub-16",lightFormat(new Date(), 'yyyy-MM-dd'),"high");
-//addTask(0,"cox cm trios", "sub-19", lightFormat(new Date(), 'yyyy-MM-dd'),"high");
-//addTask(0,"inferno", "sub-45",lightFormat(add(new Date(), {days:5}), 'yyyy-MM-dd'),"high");
-//addTask(0,"theatre of blood", "duos",lightFormat(add(new Date(), {days:24}), 'yyyy-MM-dd'),"high");
 displayDefault();
 addProjectList();
 checkToday();
 checkWeek();
-
-//localstorage
